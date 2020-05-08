3cb288f57b8f6cdfa3aa65dc75f784681bd6f28f
Roxana <roxana.c2007@gmail.com>
2020-05-08 23:07:30 +0100

    lvl 400 part update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //You can edit ALL of the code here
 function setup() {
-
   const allEpisodes = getAllEpisodes();
 
   // episodeSearchBar(allEpisodes)
@@ -9,24 +8,20 @@ function setup() {
   // matchEpisode(allEpisodes)
 
   const allShows = getAllShows();
-  selectBoxShow(allShows)
-  displayShows(allShows)
-
+  selectBoxShow(allShows);
+  displayShows(allShows);
 }
-
-
-
 
 const rootElem = document.getElementById("root");
 
 let everyEpisode = document.createElement("div");
 everyEpisode.className = "allEpisodes";
 rootElem.appendChild(everyEpisode);
-everyEpisode.innerHTML = ""
+everyEpisode.innerHTML = "";
 
 function episodeInfo(episode) {
   let eachEpisode = document.createElement("div");
-  eachEpisode.className = "oneEpisode";
+  eachEpisode.className = "oneEpisode col-12 sm-col-12 md-col-5 lg-col-3";
   everyEpisode.appendChild(eachEpisode);
 
   let seasonNumber =
@@ -43,31 +38,27 @@ function episodeInfo(episode) {
   let showSummary = document.createElement("p");
   eachEpisode.appendChild(showSummary);
   showSummary.innerHTML = episode.summary.replace("<p>", "");
-  return eachEpisode
+  return eachEpisode;
 }
 
 function makePageForEpisodes(episodeList) {
-
-  return episodeList.forEach((episode) => { episodeInfo(episode) })
-
+  return episodeList.forEach((episode) => {
+    episodeInfo(episode);
+  });
 }
 
+let searchForm = document.createElement("form");
+let targetFirstDiv = document.querySelector(".search-form");
 
-
+targetFirstDiv.appendChild(searchForm);
+let searchBar = document.createElement("input");
+searchBar.setAttribute("type", "search");
+searchBar.setAttribute("placeholder", "Search...");
+searchForm.appendChild(searchBar);
+let displayResultNumber = document.createElement("label");
+searchForm.appendChild(displayResultNumber);
 
 function episodeSearchBar(episodeList) {
-
-  let searchForm = document.createElement("form");
-  let targetFirstDiv = document.querySelector(".search-form")
-
-  targetFirstDiv.appendChild(searchForm);
-  let searchBar = document.createElement("input");
-  searchBar.setAttribute("type", "search");
-  searchBar.setAttribute("placeholder", "Search...");
-  searchForm.appendChild(searchBar);
-  let displayResultNumber = document.createElement("label");
-  searchForm.appendChild(displayResultNumber);
-
   searchBar.addEventListener("keyup", () => {
     let filteredEpisodes = episodeList.filter((episode) => {
       let lowerCaseSummary = episode.summary.toLowerCase();
@@ -77,76 +68,71 @@ function episodeSearchBar(episodeList) {
         lowerCaseSummary.includes(searchValue) ||
         lowerCaseName.includes(searchValue)
       ) {
-        return filteredEpisodes
+        return episode;
       }
     });
 
     displayResultNumber.innerHTML = `Result ${filteredEpisodes.length}/${episodeList.length}`;
-    everyEpisode.innerHTML = ``
-    return makePageForEpisodes(filteredEpisodes)
-  })
+    everyEpisode.innerHTML = ``;
+    return makePageForEpisodes(filteredEpisodes);
+  });
 }
 
-let selectEpisodeBox = document.querySelector(".select-box")
+let selectEpisodeBox = document.querySelector(".select-box");
 
 function selectBoxEpisode(episodeList) {
-  selectEpisodeBox.innerHTML += `<option value=""> </option>`
+  selectEpisodeBox.innerHTML += `<option value=""> </option>`;
 
-  episodeList.forEach(episode => {
-    let seasonNumber = episode.season > 10 ? episode.season : "0" + episode.season;
-    let episodeNumber = episode.number > 10 ? episode.number : "0" + episode.number;
-    selectEpisodeBox.innerHTML += `<option value="${episode.id}">S${seasonNumber}E${episodeNumber} - ${episode.name}</option>`
-  })
+  episodeList.forEach((episode) => {
+    let seasonNumber =
+      episode.season > 10 ? episode.season : "0" + episode.season;
+    let episodeNumber =
+      episode.number > 10 ? episode.number : "0" + episode.number;
+    selectEpisodeBox.innerHTML += `<option value="${episode.id}">S${seasonNumber}E${episodeNumber} - ${episode.name}</option>`;
+  });
 }
 
 function matchEpisode(episodeList) {
-  let selectBox = document.querySelector(".select-box")
-  let targetSelectAll = document.querySelector("#selectAll")
+  let selectBox = document.querySelector(".select-box");
+  let targetSelectAll = document.querySelector("#selectAll");
   selectBox.addEventListener("change", (e) => {
-    everyEpisode.innerHTML = ` `
+    everyEpisode.innerHTML = ` `;
     if (!event.target.value) {
-
-      return makePageForEpisodes(episodeList)
+      return makePageForEpisodes(episodeList);
     } else {
-      let newEpisodeList = episodeList.filter(episode => {
+      let newEpisodeList = episodeList.filter((episode) => {
         if (episode.id == event.target.value) {
-
-          return episode
+          return episode;
         }
-      })
-      return makePageForEpisodes(newEpisodeList)
+      });
+      return makePageForEpisodes(newEpisodeList);
     }
-  })
-
-
+  });
 }
 
 //lvl 400
 
-let selectShowBox = document.querySelector(".selectShow")
-let selectShowOption = document.querySelector("#selectAllShows")
+let selectShowBox = document.querySelector(".selectShow");
+let selectShowOption = document.querySelector("#selectAllShows");
 
 function selectBoxShow(showList) {
-  showList.forEach((show) =>
-    selectShowBox.innerHTML += `<option value="${show.id}">${show.name}</option>`)
-
+  showList.forEach(
+    (show) =>
+      (selectShowBox.innerHTML += `<option value="${show.id}">${show.name}</option>`)
+  );
 }
-
-
 
 function displayShows(showList) {
   selectShowBox.addEventListener("change", (event) => {
-    everyEpisode.innerHTML = ""
+    everyEpisode.innerHTML = "";
+
     showList.filter((show) => {
       if (event.target.value == show.id) {
-        return fetchShows(show.id)
+        return fetchShows(show.id);
       }
-    
-    })
-  })
+    });
+  });
 }
-
-
 
 function fetchShows(showId) {
   fetch(`https://api.tvmaze.com/shows/${showId}/episodes`)
@@ -154,12 +140,10 @@ function fetchShows(showId) {
       return response.json();
     })
     .then(function (data) {
-    episodeSearchBar(data)
-    makePageForEpisodes(data);
-    selectBoxEpisode(data)
-    matchEpisode(data)
-
-  });
-
+      episodeSearchBar(data);
+      makePageForEpisodes(data);
+      selectBoxEpisode(data);
+      matchEpisode(data);
+    });
 }
 window.onload = setup;
